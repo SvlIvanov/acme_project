66b81df2ce296d9dbc4a2c5e4e6bd9599e87a5ba
Sergey Ivanov <SVIvanov@yandex.ru>
2024-10-27 11:04:29 +0300

    Спринт 10 шаблоны ошибок

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_editable = (
+        'last_name',
+        'birthday',
+    )
+    search_fields = ('first_name', 'last_name',)
+    list_display_links = ('first_name',)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,120 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+    )
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+class BirthdayMixin:
+    model = Birthday
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user 
+    
+
+class BirthdayDeleteView(
+    OnlyAuthorMixin, LoginRequiredMixin, BirthdayMixin, DeleteView
+):
+    pass
+
+
+class BirthdayCreateView(LoginRequiredMixin, BirthdayMixin, CreateView):
+    form_class = BirthdayForm
+
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
+
+
+class BirthdayUpdateView(
+    OnlyAuthorMixin, LoginRequiredMixin, BirthdayMixin, UpdateView
+):
+    form_class = BirthdayForm
+
+
+class BirthdayDetailView(BirthdayMixin, DetailView):
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 5
+
+
+# def delete_birthday(request, pk):
+#     # Получаем объект модели или выбрасываем 404 ошибку.
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     # В форму передаём только объект модели;
+#     # передавать в форму параметры запроса не нужно.
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     # Если был получен POST-запрос...
+#     if request.method == 'POST':
+#         # ...удаляем объект:
+#         instance.delete()
+#         # ...и переадресовываем пользователя на страницу со списком записей.
+#         return redirect('birthday:list')
+#     # Если был получен GET-запрос — отображаем форму.
+#     return render(request, 'birthday/birthday.html', context)
+
+
+# def birthday(request, pk=None):
+#     # Если в запросе указан pk (если получен запрос на редактирование объекта):
+#     if pk is not None:
+#         # Получаем объект модели или выбрасываем 404 ошибку.
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     # Если в запросе не указан pk
+#     # (если получен запрос к странице создания записи):
+#     else:
+#         # Связывать форму с объектом не нужно, установим значение None.
+#         instance = None
+#     # Передаём в форму либо данные из запроса, либо None.
+#     # В случае редактирования прикрепляем объект модели.
+#     form = BirthdayForm(request.POST or None,
+#                         files=request.FILES or None,
+#                         instance=instance
+#                         )
+#     # Остальной код без изменений.
+#     context = {'form': form}
+#     # Сохраняем данные, полученные из формы, и отправляем ответ:
+#     if form.is_valid():
+#         form.save()
+#         birthday_countdown = calculate_birthday_countdown(
+#             form.cleaned_data['birthday']
+#         )
+#         context.update({'birthday_countdown': birthday_countdown})
+#     return render(request, 'birthday/birthday.html', context)
+
+
+# def birthday_list(request):
+#     # Получаем все объекты модели Birthday из БД.
+#     birthdays = Birthday.objects.all()
+#     paginator = Paginator(birthdays, 5)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     # Передаём их в контекст шаблона.
+#     context = {'page_obj': page_obj}
+#     return render(request, 'birthday/birthday_list.html', context)
